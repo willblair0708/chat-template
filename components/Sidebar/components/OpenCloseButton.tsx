8fd8b1223788f5b3e19e1fb2eb94048212ed7e27
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2023-08-06 20:13:38 -0700

    update OpenCloseButton

--- components/Sidebar/components/OpenCloseButton.tsx
+++ components/Sidebar/components/OpenCloseButton.tsx
@@ -9,7 +9,7 @@ export const CloseSidebarButton = ({ onClick, side }: Props) => {
   return (
     <>
       <button
-        className={`fixed top-5 ${
+        className={`fixed top-10 ${
           side === 'right' ? 'right-[270px]' : 'left-[270px]'
         } z-50 h-7 w-7 hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
           side === 'right' ? 'right-[270px]' : 'left-[270px]'
@@ -31,9 +31,9 @@ export const OpenSidebarButton = ({ onClick, side }: Props) => {
     <button
       className={`fixed top-2.5 ${
         side === 'right' ? 'right-2' : 'left-2'
-      } z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
+      } z-50 h-10 w-7 text-white hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
         side === 'right' ? 'right-2' : 'left-2'
-      } sm:h-8 sm:w-8 sm:text-neutral-700`}
+      } sm:h-10 sm:w-8 sm:text-neutral-700`}
       onClick={onClick}
     >
       {side === 'right' ? <IconArrowBarLeft /> : <IconArrowBarRight />}
